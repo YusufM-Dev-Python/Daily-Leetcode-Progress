05d30c268d99e106967a08293b0814a0f8029c08
Yusuf.M <ymotiwala19@gmail.com>
2025-12-29 14:27:58 +0000

    Day 1: Stack implementation and Valid Parentheses

---
+++ 01-Stacks/valid_parentheses.py
@@ -0,0 +1,14 @@
+# Problem: Valid Parentheses (LeetCode #20)
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
